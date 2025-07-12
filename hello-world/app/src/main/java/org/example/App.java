93c6e8e1bb9f92aa214c0345b27f3e462ece32b9
robsonoduarte <robson.o.d@gmail.com>
2025-07-12 16:37:22 -0300

    novo comentarios

--- hello-world/app/src/main/java/org/example/App.java
+++ hello-world/app/src/main/java/org/example/App.java
@@ -6,7 +6,6 @@ package org.example;
 public class App {
 
     public static void main(String[] args) {
-
         // tipos básicos
         // int - números inteiros
         // double  - números decimais
