0219b64d6a546b318e1f957045d2bf09ae03a902
Gui Farias <luizguilherme.fde@gmail.com>
2025-07-18 22:11:11 -0300

    test the commit

--- variables/app/src/main/java/org/example/App.java
+++ variables/app/src/main/java/org/example/App.java
@@ -6,7 +6,9 @@ package org.example;
 public class App {
     public static void main(String[] args) {
         int x = 1;
+        int y = 2;
         System.out.println(x);
+        System.out.println(y);
 
     }
 }
